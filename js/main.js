3e0ee7752e3b29059664f1563aee8c924c05f553
JulioVargas26 <84108359+JulioVargas26@users.noreply.github.com>
2022-12-31 17:45:19 -0500

    31/12/22

--- js/main.js
+++ js/main.js
@@ -1,44 +1,21 @@
-const botonE = document.getElementById("botonE");
-const botonD = document.getElementById("botonD");
-const botonC = document.getElementById("botonC");
-const letras = /[A-Z~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù'1-9]/g;
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })();
 
-function limpiar() {
-    document.getElementById("texto_resultado").value = "";
-    document.getElementById("texto_area").value = "";
-}
-
-function encriptar() {
-    var nuevoTexto = document.getElementById("texto_area").value;
-    nuevoTexto = nuevoTexto
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    document.getElementById("texto_resultado").value = nuevoTexto;
-}
-
-function desencriptar() {
-    var nuevoTexto = document.getElementById("texto_area").value;
-    nuevoTexto = nuevoTexto
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    document.getElementById("texto_resultado").value = nuevoTexto;
-}
-
-function copiar() {
-    document.getElementById("texto_area").placeholder = "";
-    var textCopia = document.getElementById("texto_resultado");
-    textCopia.select();
-    document.execCommand("copy");
-    limpiar();
-
-}
-
-botonE.addEventListener("click", encriptar);
-botonD.addEventListener("click", desencriptar);
-botonC.addEventListener("click", copiar);
+  
